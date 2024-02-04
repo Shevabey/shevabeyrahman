a1a16d3d4d56707bb500249f33152449926a76d5
Shevabey <Shevabey100@gmail.com>
2024-02-04 21:55:09 +0700

    update - update link blank portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import css from "./Portfolio.module.scss";
+import links from "../../utils/link";
 import { motion } from "framer-motion";
 import { fadeIn, staggerContainer } from "../../utils/motion";
 import { FaGithub, FaLink } from "react-icons/fa";
@@ -16,7 +17,10 @@ const Portfolio = () => {
       <a className="anchor" id="portfolio"></a>
       <div className={`innerWidth flexCenter ${css.container}`}>
         {/* For heading */}
-        <div className={`flexCenter ${css.heading}`}>
+        <motion.div
+          variants={fadeIn("down", "tween", 0.5, 0.6)}
+          className={`flexCenter ${css.heading}`}
+        >
           <div>
             <span className="primaryText">My Latest Work</span>
             <p style={{ marginTop: "10px" }}>
@@ -24,8 +28,15 @@ const Portfolio = () => {
             </p>
           </div>
 
-          <span className="secondaryText">Explore More Works</span>
-        </div>
+          <a
+            href={links.sosmed.github}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="secondaryText"
+          >
+            Explore More Works
+          </a>
+        </motion.div>
 
         {/* Images */}
         {/* <div className={`flexCenter ${css.showCase}`}>
@@ -43,7 +54,10 @@ const Portfolio = () => {
 
         <div className={`flexCenter ${css.portfolio}`}>
           {/* card 1 */}
-          <div className={css.card}>
+          <motion.div
+            variants={fadeIn("up", "tween", 0.5, 0.6)}
+            className={css.card}
+          >
             <img src="./mockup/mockupstore.jpg" className={css.cardImage} />
 
             <div className={css.cardContent}>
@@ -53,40 +67,63 @@ const Portfolio = () => {
                 Easy shopping with fast delivery, meeting daily home needs.
               </p>
               <div className={css.link}>
-              <div className={css.tooltip}>
-                <FaLink size={"23px"}/>
-                <div className={css.tooltiptext}>Link Demo</div>
-              </div>
-              <div className={css.tooltip}>
-                <FaGithub size={"23px"}/>
-                <div className={css.tooltiptext}>Source code</div>
-              </div>
+                <a
+                  href={links.webStoreBarokah.demo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={css.tooltip}
+                >
+                  <FaLink size={"23px"} />
+                  <div className={css.tooltiptext}>Link Demo</div>
+                </a>
+                <a
+                  href={links.webStoreBarokah.code}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={css.tooltip}
+                >
+                  <FaGithub size={"23px"} />
+                  <div className={css.tooltiptext}>Source code</div>
+                </a>
               </div>
             </div>
-          </div>
+          </motion.div>
 
           {/* card 2 */}
-          <div className={css.card}>
+          <motion.div
+            variants={fadeIn("up", "tween", 0.7, 0.6)}
+            className={css.card}
+          >
             <img src="./mockup/mockupwedding.jpg" className={css.cardImage} />
 
             <div className={css.cardContent}>
               <p className={css.cardTitle}>Wedding Website</p>
               <p className={css.cardDescription}>
-              Fulan & Fulanah`s simple, elegant, interactive wedding invitation. Reflecting the uniqueness of their marriage
+                Fulan & Fulanah`s simple, elegant, interactive wedding
+                invitation. Reflecting the uniqueness of their marriage
               </p>
               <div className={css.link}>
-              <div className={css.tooltip}>
-                <FaLink size={"23px"}/>
-                <div className={css.tooltiptext}>Link Demo</div>
-              </div>
-              <div className={css.tooltip}>
-                <FaGithub size={"23px"}/>
-                <div className={css.tooltiptext}>Source code</div>
-              </div>
+                <a
+                  href={links.webWedding.demo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={css.tooltip}
+                >
+                  <FaLink size={"23px"} />
+                  <div className={css.tooltiptext}>Link Demo</div>
+                </a>
+                <a
+                  href={links.webWedding.code}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={css.tooltip}
+                >
+                  <FaGithub size={"23px"} />
+                  <div className={css.tooltiptext}>Source code</div>
+                </a>
               </div>
             </div>
-          </div>
-          
+          </motion.div>
         </div>
       </div>
     </motion.section>
---
+++ src/utils/link.js
@@ -0,0 +1,19 @@
+const links = {
+    webStoreBarokah:{
+        demo: 'https://e-commered-barokah.netlify.app/',
+        code: 'https://github.com/Shevabey/e-commered-barokah'
+    },
+
+    webWedding: {
+        demo: 'https://wedding-invitation-olive.vercel.app/',
+        code: 'https://github.com/Shevabey/Wedding_invitation'
+    },
+
+    sosmed:{
+        github: 'https://github.com/Shevabey?tab=repositories',
+        linkdin: 'https://www.linkedin.com/in/shevabey-rahman/',
+        instagram: 'https://www.instagram.com/shevabey_rhmn/',
+    }
+}
+
+export default links;
